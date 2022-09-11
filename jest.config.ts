5a899799d35f83329fc82254f717f379e1cd0d41
Murilo Messias <murilo.messias@gmail.com>
2022-09-11 00:59:27 -0300

    merge: feature/tests (#6)

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+}
+
+export default createJestConfig(customJestConfig)
