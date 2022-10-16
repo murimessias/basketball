dbd673107b9456be94937cd1ac84dfd42176d245
Murilo Messias de Camargo <murilo.messias@gmail.com>
2022-10-16 10:51:20 -0300

    refactor: add types based on backend response

--- src/types/team.ts
+++ src/types/team.ts
@@ -1,8 +1,17 @@
-export type Team = {
+type Conference = 'East' | 'West'
+type Division =
+  | 'Atlantic'
+  | 'Central'
+  | 'Northwest'
+  | 'Pacific'
+  | 'Southeast'
+  | 'Southwest'
+
+export interface Team {
   abbreviation: string
   city: string
-  conference: string
-  division: string
+  conference: Conference
+  division: Division
   full_name: string
   id: number
   name: string
